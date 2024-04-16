a6d4b5c1a66cd110dc926c307af5a94c43424aaa
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-04-17 00:33:40 +0530

    reducers and context

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,15 @@
 import {Link} from "react-router-dom"
-
 import React from 'react'
 import "./Navbar.css"
 import Searchbar from "./Searchbar"
+import {useTheme} from '../hooks/useTheme'
+
 export default function Navbar() {
+  const {color,changeColor}=useTheme()  
+
   return (
-    <div className='navbar'>
-         <nav>
+    <div className='navbar' style={{background:color}}> 
+         <nav onClick={()=>{changeColor('pink')}}>
          <Link exact to="/" className="brand">
              <h1>Cooking Ninja</h1>
          </Link>
--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -1,7 +1,32 @@
-import { createContext } from "react";
+import { createContext,useReducer } from "react";
 
 import React from 'react'
 
 export const  ThemeContext=createContext()
 
-<ThemeContext.Provider>
+const themeReducer=(state,action)=>{
+    switch(action.type){
+        case 'CHANGE_COLOR':
+         return {...state,color:action.payload}
+        default:
+         return state
+    }
+}
+
+
+export function ThemeProvider ({children}){
+   //now we can add some custom logic and easily able to change it
+     const [state,dispatch]= useReducer(themeReducer,{
+      color:'blue'
+     })
+     
+     const changeColor=(color)=>{
+         dispatch({type:'CHANGE_COLOR',payload:color})
+     }
+
+     return (
+        <ThemeContext.Provider value={{...state,changeColor}}>
+            {children}
+        </ThemeContext.Provider>
+     )
+}
